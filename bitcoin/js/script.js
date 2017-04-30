bb14d4e0c0311af52265e74ab52f614b6798466b
Laurawehbe <lwehbe.pro@gmail.com>
2017-04-30 15:01:10 +0200

    Update script.js

--- bitcoin/js/script.js
+++ bitcoin/js/script.js
@@ -1,56 +1,14 @@
 div 
 {
- public class main 
-    {
-        public static void main(String[] args) throws MalformedURLException, IOException
-        {
-            JSONObject data = getJSONfromURL("https://blockchain.info/charts/market-price?format=json");
-            JSONArray data_array = data.getJSONArray("values");
 
-            for (int i = 0; i < data_array.length(); i++)
-            {
-                JSONObject price_point = data_array.getJSONObject(i);
-
-                //  Unix time
-                int x = price_point.getInt("x");
-
-                //  Bitcoin price at that time
-                double y = price_point.getDouble("y");
-
-                //  Do something with x and y.
-            }
-
-        }
-
-        public static JSONObject getJSONfromURL(String URL)
-        {
-            try
-            {
-                URLConnection uc;
-                URL url = new URL(URL);
-                uc = url.openConnection();
-                uc.setConnectTimeout(10000);
-                uc.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-                uc.connect();
-
-                BufferedReader rd = new BufferedReader(
-                        new InputStreamReader(uc.getInputStream(), 
-                        Charset.forName("UTF-8")));
-
-                StringBuilder sb = new StringBuilder();
-                int cp;
-                while ((cp = rd.read()) != -1)
-                {
-                    sb.append((char)cp);
-                }
-
-                String jsonText = (sb.toString());            
-
-                return new JSONObject(jsonText.toString());
-            } catch (IOException ex)
-            {
-                return null;
-            }
-        }
+    var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo").innerHTML = jsonPretty;
     }
+};
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.send();
 }
